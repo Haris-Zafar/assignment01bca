b498456e7f166d29adc66eaaaef18861079a1fcb
Haris-Zafar <115533881+Haris-Zafar@users.noreply.github.com>
2023-10-09 20:52:44 +0500

    added main package

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	a1 "github.com/Haris-Zafar/assignment01bca"
+)
+
+func main() {
+	genesisBlock := a1.NewBlock("Genesis Block", 0, "")
+
+	block1 := a1.NewBlock("Alice to Bob", 12345, genesisBlock.CurrentHash)
+	block2 := a1.NewBlock("Bob to Carol", 67890, block1.CurrentHash)
+
+	genesisBlock.NextBlock = block1
+	block1.NextBlock = block2
+
+	a1.DisplayBlocks(genesisBlock)
+
+	a1.ChangeBlock(block1, "Modified Transaction in Block 1")
+
+	isValid := a1.VerifyChain(genesisBlock)
+	if isValid {
+		fmt.Println("Blockchain is valid.")
+	} else {
+		fmt.Println("Blockchain is not valid.")
+	}
+}
